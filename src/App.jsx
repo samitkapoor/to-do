32149cf5c76c5a3f17c903bcd48bd2d9101d8200
samitkapoor <samitkapoor77@gmail.com>
2023-05-24 14:20:01 +0530

    Data persistence on reload page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import MyForm from "./components/MyForm";
 import MyTasks from "./components/MyTasks";
@@ -7,7 +7,16 @@ import "./styles/app.css";
 
 export default function App() {
   const [newItem, setNewItem] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(() => {
+    console.log("hey");
+    const myItems = localStorage.getItem("ITEMS");
+    if (myItems == null) return [];
+    return JSON.parse(myItems);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todoList));
+  }, [todoList]);
 
   function changeNewItem(e) {
     e.preventDefault();
@@ -16,17 +25,20 @@ export default function App() {
 
   function addNewItem(e) {
     e.preventDefault();
-    if(newItem != "")
-      setTodoList([...todoList, { id: crypto.randomUUID(), title: newItem, completed: false }]);
-    
+    if (newItem != "")
+      setTodoList([
+        ...todoList,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]);
+
     setNewItem("");
   }
 
   function toggleCompleteCheck(key, completed) {
-    setTodoList(currentTodo => { 
-      return currentTodo.map(todoItem => { 
+    setTodoList((currentTodo) => {
+      return currentTodo.map((todoItem) => {
         if (todoItem.id === key) {
-          return {...todoItem, completed}
+          return { ...todoItem, completed };
         }
         return todoItem;
       });
@@ -34,16 +46,26 @@ export default function App() {
   }
 
   function deleteItem(key) {
-    setTodoList(currentTodo => {
-      return currentTodo.filter(todoItem => todoItem.id != key)
-    })
+    setTodoList((currentTodo) => {
+      return currentTodo.filter((todoItem) => todoItem.id != key);
+    });
   }
 
-  return <div className="background">
-    <div className="opaque-bg"></div>
-    <div className="app">
-      <MyForm newItem={newItem} onSubmit={addNewItem} onChange={changeNewItem}></MyForm>
-      <MyTasks todoList={todoList} onToggleComplete={toggleCompleteCheck} onDelete={deleteItem}></MyTasks>
+  return (
+    <div className="background">
+      <div className="opaque-bg"></div>
+      <div className="app">
+        <MyForm
+          newItem={newItem}
+          onSubmit={addNewItem}
+          onChange={changeNewItem}
+        ></MyForm>
+        <MyTasks
+          todoList={todoList}
+          onToggleComplete={toggleCompleteCheck}
+          onDelete={deleteItem}
+        ></MyTasks>
+      </div>
     </div>
-  </div>
+  );
 }
